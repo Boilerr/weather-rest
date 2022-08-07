f01b9c78f25d5117ab9c50df05a5ff9bd7564e70
Boilerr <sergey.nys@gmail.com>
2022-08-07 19:09:30 +0300

    Fully working Spring REST, ready to add functionality

---
+++ src/main/java/com/boilerr/weatherrest/WeatherRestApplication.java
@@ -0,0 +1,13 @@
+package com.boilerr.weatherrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boilerr/weatherrest/WeatherRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boilerr.weatherrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
