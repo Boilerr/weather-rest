1aaf3104f14571b1ad3fc880fa49dfac7761c719
Boilerr <sergey.nys@gmail.com>
2022-08-13 22:05:36 +0300

    Add Tests

--- src/test/java/com/boilerr/weatherrest/WeatherRestApplicationTests.java
+++ src/test/java/com/boilerr/weatherrest/WeatherRestApplicationTests.java
@@ -3,6 +3,8 @@ package com.boilerr.weatherrest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
 @SpringBootTest
 class WeatherRestApplicationTests {
 
@@ -26,6 +28,31 @@ class WeatherRestApplicationTests {
 		public void paramGreetingShouldReturnTailoredMessage() throws Exception {
 
 		}
+	
+
+
+
+
+
+
+	@Test
+	void contextLoads() {
+	}
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
+
+		this.mockMvc.perform(get("/color")).andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.name").value("Color, Blue!"));
 	}
 
+	@Test
+	public void paramGreetingShouldReturnTailoredMessage() throws Exception {
+
+		this.mockMvc.perform(get("/color").param("name", "White"))
+				.andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.name").value("Color, White!"));
+	}
 }
