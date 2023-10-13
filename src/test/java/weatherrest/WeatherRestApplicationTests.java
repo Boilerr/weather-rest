beafd5b70b1b70702ed1cca29cef6e0e383ef014
Boilerr <sergey.nys@gmail.com>
2023-10-13 17:59:38 +0300

    Add Spring Boot 3.1.4 template from start.spring.io

---
+++ src/test/java/weatherrest/WeatherRestApplicationTests.java
@@ -0,0 +1,13 @@
+package weatherrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
