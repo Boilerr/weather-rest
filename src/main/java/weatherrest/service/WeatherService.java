0f0694c0203d2ff1f83877d666a7aad56891cdf8
Boilerr <sergey.nys@gmail.com>
2023-11-19 19:38:22 +0200

    Migrate endpoints to Service Layer

--- src/main/java/weatherrest/service/WeatherService.java
+++ src/main/java/weatherrest/service/WeatherService.java
@@ -1,10 +1,14 @@
 package weatherrest.service;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import weatherrest.model.WeatherData;
 import weatherrest.exception.WeatherDataAlreadyExistsException;
 
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WeatherService {
 
@@ -13,4 +17,15 @@ public interface WeatherService {
     WeatherData getWeatherDataById(Long id);
     WeatherData deleteWeatherDataById(Long id);
 
+    List<WeatherData> findAll();
+
+    Page<WeatherData> getAllWeatherWithPageable(Pageable pageable);
+
+    Page<WeatherData> getAllWeatherWithPageableAndSorting();
+
+    Optional<WeatherData> getOneWeatherDataById(Long weatherId);
+
+    List<WeatherData> getWeathersByExample(Example<WeatherData> example);
+
+    WeatherData saveNewWeatherData(WeatherData weatherData);
 }
--- src/main/java/weatherrest/service/WeatherServiceImpl.java
+++ src/main/java/weatherrest/service/WeatherServiceImpl.java
@@ -1,6 +1,9 @@
 package weatherrest.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import weatherrest.exception.WeatherDataAlreadyExistsException;
 import weatherrest.model.WeatherData;
@@ -49,5 +52,35 @@ public class WeatherServiceImpl implements WeatherService {
             weatherRepository.deleteById(id);
         }
         return product;
+    }
+
+    @Override
+    public List<WeatherData> findAll() {
+        return null;
+    }
+
+    @Override
+    public Page<WeatherData> getAllWeatherWithPageable(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Page<WeatherData> getAllWeatherWithPageableAndSorting() {
+        return null;
+    }
+
+    @Override
+    public Optional<WeatherData> getOneWeatherDataById(Long weatherId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<WeatherData> getWeathersByExample(Example<WeatherData> example) {
+        return null;
+    }
+
+    @Override
+    public WeatherData saveNewWeatherData(WeatherData weatherData) {
+        return null;
     }
 }
