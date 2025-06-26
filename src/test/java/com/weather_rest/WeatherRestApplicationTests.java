644622086027e5fbc7785966a81bf1397b68bd0a
Boilerr <sergey.nys@gmail.com>
2025-06-26 18:22:39 +0300

    Add Spring Boot 3.5.3 with dependencies

---
+++ src/test/java/com/weather_rest/WeatherRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
